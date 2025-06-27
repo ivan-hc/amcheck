ecba86b3a9eb4b87b8e5b5d97cc594f3ed4aeebe
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-06-27 12:39:18 +0200

    Delete .github/scripts directory

--- .github/scripts/count.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Excluding: kdegames kdeutils node platform-tools ffwa-" >> $GITHUB_STEP_SUMMARY
-sort programs/x86_64-apps | grep -v "\"kdegames\"\|\"kdeutils\"\|\"node\"\|\"platform-tools\"\| ffwa-" | awk '{print $2}' > appslist
-x64Count=$(wc -l < appslist)
-echo "all=$x64Count" >> $GITHUB_OUTPUT
